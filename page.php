1ad9617931f247b3db0b5d7ac90a2a082ba5297f
NAMERTEIZM <109963581+NAMERTEIZM@users.noreply.github.com>
2022-07-27 21:36:09 +0300

    Add files via upload

---
+++ page.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <title>tusiad</title>
+</head>
+<body>
+    <div class="container" id="login-container">
+   
+    <div class="row">
+   
+      
+<div class="col-xl-12" id="deneme">
+    
+    <img class="img-fluid" id="bina" src="img/bina.png">
+</img>
+
+<div id="butonclass" class="col-xl-3">
+<btn class="btn btn-warning" id="buton" name="button">TÜSİAD'ı ziyaret edin</btn>
+
+</div>
+
+<div class="col-xl-5" id="yazilar">
+ 
+<a id= "baslik" class="text-xl-right">TÜSİAD & GİRİŞİMCİLİK</a>
+<p>TÜSİAD, girişimciliğe 360 derecelik bir bakış açısıyla yaklaşarak bu alanda
+Türkiye’nin rekabet gücünün artmasını  amaçlamaktadır.</p>
+
+<p>Bu çerçevede bir yandan genç girişimciliğini ve yenilikçi girişimcileri, diğer
+yandan kurumların girişimcilik dönüşümünü desteklemeye yönelik belediyeler
+dahil pek çok paydaş ile çalışmalar yürütmekte ve girişimcilik ekosisteminin
+gelişimi için projeler geliştirmektedir.</p>
+
+</div>
+  <!--  <h6 id= "baslik" class="text-xl-right">TÜSİAD & GİRİŞİMCİLİK</h6> -->
+  </div>
+   
+</div>
+
+ 
+    </div>
+    
+</body>
+</html>
